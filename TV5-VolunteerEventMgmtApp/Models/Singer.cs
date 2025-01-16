3d1920b2a0988f895e8d5b1cffdd5cc8a6205566
Zach <88806017+Zachzzz@users.noreply.github.com>
2025-01-16 18:55:36 -0500

    Added id property (forgot)

--- TV5-VolunteerEventMgmtApp/Models/Singer.cs
+++ TV5-VolunteerEventMgmtApp/Models/Singer.cs
@@ -4,16 +4,18 @@ namespace TV5_VolunteerEventMgmtApp.Models
 {
     public class Singer
     {
+        public int Id { get; set; } //
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateOnly DOB { get; set; }
 
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
-
+        //
         [DataType(DataType.PhoneNumber)]
         public string? Phone {  get; set; }
 
         bool isActive { get; set; } = true;
+        //
     }
 }
