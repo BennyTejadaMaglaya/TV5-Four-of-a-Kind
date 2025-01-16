2d5a179c72f6e4fba8807e81f7c5a85c71a4036b
fraser kennedy <ydennekrf@gmail.com>
2025-01-16 18:14:34 -0500

    added migrations to Program.cs added the AppInit and the DBContext

--- TV5-VolunteerEventMgmtApp/Data/VolunteerEventMgmtAppDbContext.cs
+++ TV5-VolunteerEventMgmtApp/Data/VolunteerEventMgmtAppDbContext.cs
@@ -1,13 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
+using TV5_VolunteerEventMgmtApp.Models;
 
 namespace TV5_VolunteerEventMgmtApp.Data
 {
     public class VolunteerEventMgmtAppDbContext : DbContext
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public string UserName
+        {
+            get; private set;
+        }
+        public VolunteerEventMgmtAppDbContext(DbContextOptions<VolunteerEventMgmtAppDbContext> options, IHttpContextAccessor httpContextAccessor)
+            : base(options)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                //We have a HttpContext, but there might not be anyone Authenticated
+                UserName = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "Unknown";
+            }
+            else
+            {
+                //No HttpContext so seeding data
+                UserName = "Seed Data";
+            }
+        }
         public VolunteerEventMgmtAppDbContext(DbContextOptions<VolunteerEventMgmtAppDbContext> options)
             : base(options)
         {
+            _httpContextAccessor = null!;
+            UserName = "Seed Data";
         }
     }
 }
