5efeee9ad5c590a54be55a967f44357376adff5e
Zach <88806017+Zachzzz@users.noreply.github.com>
2025-01-16 19:39:26 -0500

    Attendance Annotations

--- TV5-VolunteerEventMgmtApp/Models/AttendanceSheet.cs
+++ TV5-VolunteerEventMgmtApp/Models/AttendanceSheet.cs
@@ -1,4 +1,6 @@
-﻿namespace TV5_VolunteerEventMgmtApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TV5_VolunteerEventMgmtApp.Models
 {
     public class AttendanceSheet
     {
@@ -7,7 +9,9 @@
         public int DirectorId { get; set; }
         public ICollection<Attendee> Attendees { get; set; } = new HashSet<Attendee>();
         public string Notes { get; set; } = "";
+        [Display(Name = "Start Time")]
         public DateTime StartTime { get; set; }
+        [Display(Name = "End Time")]
         public DateTime EndTime { get; set; }
     }
 }
--- TV5-VolunteerEventMgmtApp/Models/Singer.cs
+++ TV5-VolunteerEventMgmtApp/Models/Singer.cs
@@ -5,18 +5,23 @@ namespace TV5_VolunteerEventMgmtApp.Models
     public class Singer
     {
         public int Id { get; set; }
- 
+
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "DOB")]
         public DateOnly DOB { get; set; }
 
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+        [Display(Name = "Phone Number")]
         [DataType(DataType.PhoneNumber)]
         public string? Phone {  get; set; }
 
         public ICollection<Attendee> Attendance {  get; set; } = new HashSet<Attendee>();
-        
+
+        [Display(Name = "Active")]
         bool isActive { get; set; } = true;
     }
 }
