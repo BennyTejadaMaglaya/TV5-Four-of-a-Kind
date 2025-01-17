31055775865318e132375f93e72f8240ab0fa032
Zach <88806017+Zachzzz@users.noreply.github.com>
2025-01-16 19:23:14 -0500

    attendance

---
+++ TV5-VolunteerEventMgmtApp/Models/AttendanceSheet.cs
@@ -0,0 +1,13 @@
+﻿namespace TV5_VolunteerEventMgmtApp.Models
+{
+    public class AttendanceSheet
+    {
+
+        public int Id { get; set; }
+        public int DirectorId { get; set; }
+        public ICollection<Attendee> Attendees { get; set; } = new HashSet<Attendee>();
+        public string Notes { get; set; } = "";
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ TV5-VolunteerEventMgmtApp/Models/Attendee.cs
@@ -0,0 +1,11 @@
+﻿namespace TV5_VolunteerEventMgmtApp.Models
+{
+    public class Attendee
+    {
+        public int Id { get; set; }
+        public Singer? Singer { get; set; }
+        public int SingerId { get; set; }
+        public int AttendenceSheetId { get; set; }
+        public AttendanceSheet? AttendanceSheet { get; set; }
+    }
+}
