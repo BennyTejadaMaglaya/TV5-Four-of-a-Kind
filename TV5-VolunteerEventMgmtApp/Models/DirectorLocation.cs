8f96fb9fc890e426954af88984a6bc83eecc40f1
fraser kennedy <ydennekrf@gmail.com>
2025-01-16 19:51:28 -0500

    added model for director and director locations to accomidate the many to many for directors and locations.

---
+++ TV5-VolunteerEventMgmtApp/Models/DirectorLocation.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace TV5_VolunteerEventMgmtApp.Models
+{
+    public class DirectorLocation
+    {
+        public int DirectorID { get; set; }
+        public Director? Director { get; set; }
+
+        public int LocationID { get; set; }
+        
+        public Location? Location { get; set; }
+    }
+}
