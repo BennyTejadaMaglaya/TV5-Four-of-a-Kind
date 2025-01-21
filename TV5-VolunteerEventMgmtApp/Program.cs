f0bfb6a0f454531c15f6fcf5944fe83950eacdbc
Zach <88806017+Zachzzz@users.noreply.github.com>
2025-01-21 18:35:52 -0500

    Added a new dependancy to the project and implemented the csv import

--- TV5-VolunteerEventMgmtApp/Program.cs
+++ TV5-VolunteerEventMgmtApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TV5_VolunteerEventMgmtApp.Data;
+using TV5_VolunteerEventMgmtApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton(new CsvService());
 
 var app = builder.Build();
 
