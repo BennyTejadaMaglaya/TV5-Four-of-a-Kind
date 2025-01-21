d04e16f932507d6cd337b1a3a26212bd727e193d
Zach <88806017+Zachzzz@users.noreply.github.com>
2025-01-21 12:49:26 -0500

    Fix

--- TV5-VolunteerEventMgmtApp/Controllers/SummaryController.cs
+++ TV5-VolunteerEventMgmtApp/Controllers/SummaryController.cs
@@ -24,29 +24,32 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> Attendance(DateTime? startWeek, DateTime? endWeek)
+        public async Task<IActionResult> Attendance(
+                                    DateTime? startWeek, 
+                                    DateTime? endWeek,     
+                                    string? actionButton,
+                                    string? searchCity,
+                                    string? searchDirector,
+                                    string sortDirection = "asc",
+                                    string sortField = "City")
         {
-            // This summary grabs all sheets created this week for a quick overview 
-            // get each loctions attendance
-            // get total active singers (AttendanceSheet.SingerLocations.Count where singer is active)
-           
-            //sort by week steps
-            // 1. take in a date and get the weekstart from that date
-            // 2. filter by that week instead of the default this week
-
-            // if theres no start week and no end week we default to this week otherwise we use the range
-
+            /* TODOS
+             * Sort by total attendees asc/desc
+             * sort by total available singers asc/desc
+             */
             
             var summary = _context.AttendeesSheets
                         .Include(i => i.Attendees)
                         .Include(a => a.Location)
                         .ThenInclude(l => l.SingerLocations)
                         .ThenInclude(l => l.Singer)
-                        
+                        .Include(l => l.Director)
                         .AsNoTracking();
 
             FilterDateRange(startWeek, endWeek, ref summary);
-
+            SortUtilities.SwapSortDirection(ref sortField, ref sortDirection, ["City", "Director"], actionButton);
+            SortAttendanceSheets(ref summary, sortField, sortDirection);
+            
             if (startWeek.HasValue)
             {
                 ViewData["startWeek"] = startWeek.Value;
@@ -62,15 +65,25 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
             {
                 TotalAttendees = x.Location.SingerLocations.Where(l => l.Singer.isActive).Count(),
                 Sheet = x,
-                Percentage = new PercentageColor((double)x.Attendees.Count() / (double)x.Location.SingerLocations.Where(l => l.Singer.isActive).Count())
+                Percentage = new PercentageColorVM((double)x.Attendees.Count() / x.Location.SingerLocations.Where(l => l.Singer.isActive).Count())
             })
-                        .ToListAsync();
+            .ToListAsync();
+
 
             ViewData["summaryTitle"] = (!startWeek.HasValue && !endWeek.HasValue) ? "This Weeks Attendance Summary" 
                 : $"Attendance Summary {SummaryUtilities.DateRangeMessage(startWeek, endWeek)}";
-
-            // future:
-            // Allow searching/filtering for location on this page
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+            ViewData["searchCity"] = searchCity;
+            ViewData["searchDirector"] = searchDirector;
+            if (startWeek.HasValue) // todo fix this ( think i
+            {
+                ViewData["startWeek"] = startWeek.Value.ToString("yyyy-MM-dd");
+            }
+            if (endWeek.HasValue)
+            {
+                ViewData["endWeek"] = endWeek.Value.ToString("yyyy-MM-dd");
+            }
 
             return View(summaryValue);
 
@@ -88,14 +101,31 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
             if (start.HasValue)
             {
                 sheets = sheets.Where(s => s.StartTime >= start);
-                Console.WriteLine(sheets.Count().ToString() + " Start");
+                //Console.WriteLine(sheets.Count().ToString() + " Start");
             }
             if (end.HasValue)
             { 
                 sheets = sheets.Where(sheets => sheets.StartTime <= end.Value);
-                Console.WriteLine(sheets.Count().ToString() + " End");
+                //Console.WriteLine(sheets.Count().ToString() + " End");
             }
             
+        }
+
+        private static void SortAttendanceSheets(ref IQueryable<AttendanceSheet> sheets, string sortField, string sortDirection)
+        {
+            if (sortField == "Director")
+            {
+                sheets = sortDirection == "asc" ?
+                    sheets.OrderByDescending(s => s.Director.FirstName).ThenByDescending(s => s.Director.LastName)
+                    :
+                    sheets.OrderBy(s => s.Director.FirstName).ThenBy(s => s.Director.LastName);
+            }
+
+            if (sortField == "City")
+            {
+                sheets = sortDirection == "asc" ?
+                    sheets.OrderByDescending(s => s.Location.City) : sheets.OrderBy(s => s.Location.City);
+            }
         }
 
 
---
+++ TV5-VolunteerEventMgmtApp/ViewModels/PercentageColorVM.cs
@@ -0,0 +1,38 @@
+﻿namespace TV5_VolunteerEventMgmtApp.ViewModels
+{
+    public class PercentageColorVM
+    {
+        public const string GreatColor = "#44ce1b";
+        public const string GoodColor = "#bbdb44";
+        public const string PoorColor = "#f2a134";
+        public const string BadColor = "#e51f1f";
+
+        public const string GreatClassName = "text-summary-great";
+        public const string GoodClassName = "text-summary-good";
+        public const string PoorClassName = "text-summary-poor";
+        public const string BadClassName = "text-summary-bad";
+
+        public string ClassName { get; set; }
+        public double Value { get; set; }
+        public PercentageColorVM(double summaryPercent)
+        {
+            if (summaryPercent >= 0.75)
+            {
+                ClassName = GreatClassName;
+            }
+            if (summaryPercent < 0.75 && summaryPercent > 0.60)
+            {
+                ClassName = GoodClassName;
+            }
+            if (summaryPercent <= 0.60 && summaryPercent >= 0.50)
+            {
+                ClassName = PoorClassName;
+            }
+
+            if (summaryPercent < 0.50) { ClassName = BadClassName; }
+            Value = summaryPercent;
+        }
+
+
+    }
+}
