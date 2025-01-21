6467fb827920b6fc73297a8b56685b156745ab33
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2025-01-21 02:35:54 -0500

    Change some styles

--- TV5-VolunteerEventMgmtApp/Controllers/AttendanceSheetController.cs
+++ TV5-VolunteerEventMgmtApp/Controllers/AttendanceSheetController.cs
@@ -11,189 +11,198 @@ using TV5_VolunteerEventMgmtApp.ViewModels;
 
 namespace TV5_VolunteerEventMgmtApp.Controllers
 {
-    public class AttendanceSheetController : Controller
-    {
-        private readonly VolunteerEventMgmtAppDbContext _context;
+	public class AttendanceSheetController : Controller
+	{
+		private readonly VolunteerEventMgmtAppDbContext _context;
 
-        public AttendanceSheetController(VolunteerEventMgmtAppDbContext context)
-        {
-            _context = context;
-        }
+		public AttendanceSheetController(VolunteerEventMgmtAppDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: AttendanceSheet
-        public async Task<IActionResult> Index()
-        {
-            var volunteerEventMgmtAppDbContext = _context.AttendeesSheets
-                .Include(a => a.Director)
-                .Include(a => a.Location)
-                
-                .Include(a => a.Attendees);
-            return View(await volunteerEventMgmtAppDbContext.ToListAsync());
-        }
+		// GET: AttendanceSheet
+		public async Task<IActionResult> Index()
+		{
+			var volunteerEventMgmtAppDbContext = _context.AttendeesSheets
+				.Include(a => a.Director)
+				.Include(a => a.Location)
+				.Include(a => a.Attendees)
+				.OrderByDescending(a => a.StartTime);
+			return View(await volunteerEventMgmtAppDbContext.ToListAsync());
+		}
 
-        // GET: AttendanceSheet/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: AttendanceSheet/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var attendanceSheet = await _context.AttendeesSheets
-                .Include(a => a.Director)
-                .Include(a => a.Location)
-                
-                .Include(a => a.Attendees).ThenInclude(a => a.Singer)
+			var attendanceSheet = await _context.AttendeesSheets
+				.Include(a => a.Director)
+				.Include(a => a.Location)
+
+				.Include(a => a.Attendees).ThenInclude(a => a.Singer)
 				.FirstOrDefaultAsync(m => m.Id == id);
-            if (attendanceSheet == null)
-            {
-                return NotFound();
-            }
+			if (attendanceSheet == null)
+			{
+				return NotFound();
+			}
 
-            return View(attendanceSheet);
-        }
+			return View(attendanceSheet);
+		}
 
-        // GET: AttendanceSheet/Create
-        public IActionResult Create()
-        {
-			AttendanceSheet attendanceSheet = new AttendanceSheet();
+		// GET: AttendanceSheet/Create
+		public IActionResult Create()
+		{
+			DateTime now = DateTime.Now;
+			DateTime thisHour = new DateTime(now.Year, now.Month, now.Day, now.Hour, 0, 0);
+
+			AttendanceSheet attendanceSheet = new AttendanceSheet()
+			{
+				StartTime = thisHour,
+				EndTime = thisHour.AddHours(1) // End time is one hour after start time
+			};
+
 			ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName");
-            ViewData["LocationID"] = new SelectList(_context.Locations, "ID", "City");
-           
+			ViewData["LocationID"] = new SelectList(_context.Locations, "ID", "City");
+
 			//PopulateSingerListBoxes(attendanceSheet);
 			// Empty lists at first, then AvailableSingers will be populated when the location is selected
 			ViewData["SelectedSingers"] = new MultiSelectList(Enumerable.Empty<ListOptionVM>(), "ID", "DisplayText");
 			ViewData["AvailableSingers"] = new MultiSelectList(Enumerable.Empty<ListOptionVM>(), "ID", "DisplayText");
 
-			return View();
-        }
-
-        // POST: AttendanceSheet/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,DirectorId,Notes,StartTime,EndTime,LocationId,VenueId")] AttendanceSheet attendanceSheet,
-			string[] selectedOptions)
-        {
-            UpdateAttendees(selectedOptions, attendanceSheet);
-            if (ModelState.IsValid)
-            {
-                _context.Add(attendanceSheet);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName", attendanceSheet.DirectorId);
-            ViewData["LocationId"] = new SelectList(_context.Locations, "ID", "City", attendanceSheet.LocationId);
-            
-            PopulateSingerListBoxes(attendanceSheet);
 			return View(attendanceSheet);
-        }
 
-        // GET: AttendanceSheet/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		}
 
-            var attendanceSheet = await _context.AttendeesSheets
-                .Include(a => a.Attendees).ThenInclude(a => a.Singer)
-                .FirstOrDefaultAsync(a => a.Id == id);
-            if (attendanceSheet == null)
-            {
-                return NotFound();
-            }
-            ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName", attendanceSheet.DirectorId);
-            ViewData["LocationId"] = new SelectList(_context.Locations, "ID", "City", attendanceSheet.LocationId);
-           
+		// POST: AttendanceSheet/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,DirectorId,Notes,StartTime,EndTime,LocationId,VenueId")] AttendanceSheet attendanceSheet,
+			string[] selectedOptions)
+		{
+			UpdateAttendees(selectedOptions, attendanceSheet);
+			if (ModelState.IsValid)
+			{
+				_context.Add(attendanceSheet);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName", attendanceSheet.DirectorId);
+			ViewData["LocationId"] = new SelectList(_context.Locations, "ID", "City", attendanceSheet.LocationId);
+
 			PopulateSingerListBoxes(attendanceSheet);
 			return View(attendanceSheet);
-        }
+		}
 
-        // POST: AttendanceSheet/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, string[] selectedOptions)
-        {
+		// GET: AttendanceSheet/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var attendanceSheet = await _context.AttendeesSheets
+				.Include(a => a.Attendees).ThenInclude(a => a.Singer)
+				.FirstOrDefaultAsync(a => a.Id == id);
+			if (attendanceSheet == null)
+			{
+				return NotFound();
+			}
+			ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName", attendanceSheet.DirectorId);
+			ViewData["LocationId"] = new SelectList(_context.Locations, "ID", "City", attendanceSheet.LocationId);
+
+			PopulateSingerListBoxes(attendanceSheet);
+			return View(attendanceSheet);
+		}
+
+		// POST: AttendanceSheet/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, string[] selectedOptions)
+		{
 			var attendanceSheetToUpdate = await _context.AttendeesSheets
 				.Include(a => a.Attendees).ThenInclude(a => a.Singer)
 				.FirstOrDefaultAsync(a => a.Id == id);
 
 			if (id != attendanceSheetToUpdate.Id)
-            {
-                return NotFound();
+			{
+				return NotFound();
 			}
 
 			UpdateAttendees(selectedOptions, attendanceSheetToUpdate);
 
 			if (await TryUpdateModelAsync<AttendanceSheet>(attendanceSheetToUpdate, "",
-		        a => a.DirectorId, a => a.Notes, a => a.StartTime, 
-                a => a.EndTime, a => a.LocationId))
+				a => a.DirectorId, a => a.Notes, a => a.StartTime,
+				a => a.EndTime, a => a.LocationId))
 			{
-                try
-                {
-                    await _context.SaveChangesAsync();
+				try
+				{
+					await _context.SaveChangesAsync();
 					return RedirectToAction("Details", new { id = attendanceSheetToUpdate.Id });
 				}
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AttendanceSheetExists(attendanceSheetToUpdate.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-            }
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!AttendanceSheetExists(attendanceSheetToUpdate.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+			}
 
-            ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName", attendanceSheetToUpdate.DirectorId);
-            ViewData["LocationId"] = new SelectList(_context.Locations, "ID", "City", attendanceSheetToUpdate.LocationId);
-           
+			ViewData["DirectorId"] = new SelectList(_context.Directors, "ID", "FullName", attendanceSheetToUpdate.DirectorId);
+			ViewData["LocationId"] = new SelectList(_context.Locations, "ID", "City", attendanceSheetToUpdate.LocationId);
+
 			PopulateSingerListBoxes(attendanceSheetToUpdate);
 			return View(attendanceSheetToUpdate);
-        }
+		}
 
-        // GET: AttendanceSheet/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: AttendanceSheet/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var attendanceSheet = await _context.AttendeesSheets
-                .Include(a => a.Director)
-                .Include(a => a.Location)
-                
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (attendanceSheet == null)
-            {
-                return NotFound();
-            }
+			var attendanceSheet = await _context.AttendeesSheets
+				.Include(a => a.Director)
+				.Include(a => a.Location)
 
-            return View(attendanceSheet);
-        }
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (attendanceSheet == null)
+			{
+				return NotFound();
+			}
 
-        // POST: AttendanceSheet/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var attendanceSheet = await _context.AttendeesSheets.FindAsync(id);
-            if (attendanceSheet != null)
-            {
-                _context.AttendeesSheets.Remove(attendanceSheet);
-            }
+			return View(attendanceSheet);
+		}
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: AttendanceSheet/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var attendanceSheet = await _context.AttendeesSheets.FindAsync(id);
+			if (attendanceSheet != null)
+			{
+				_context.AttendeesSheets.Remove(attendanceSheet);
+			}
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
 
 		// Populate Singer ListBoxes
@@ -269,80 +278,80 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
 		}
 
 
-        public async Task<IActionResult> GetSingersByLocation(int locationId)
-        {
-            var singers = await _context.Singers
-                .Where(s => s.SingerLocation.Any(sl => sl.LocationId == locationId))
-                .Select(s => new { s.Id, s.FirstName, s.LastName })
-                .OrderBy(s => s.FirstName).ThenBy(s => s.LastName)
+		public async Task<IActionResult> GetSingersByLocation(int locationId)
+		{
+			var singers = await _context.Singers
+				.Where(s => s.SingerLocation.Any(sl => sl.LocationId == locationId))
+				.Select(s => new { s.Id, s.FirstName, s.LastName })
+				.OrderBy(s => s.FirstName).ThenBy(s => s.LastName)
 				.ToListAsync();
 
-            return Json(singers);
-        }
+			return Json(singers);
+		}
 
-        public async Task<JsonResult> GetAttendanceByLocation(int locationId)
-        {
-            var attendanceHistory = await _context.AttendeesSheets
-                .Include(a => a.Director)
-                .Include(a => a.Location)
-                
-                .Include(a => a.Attendees)
-                .Where(a => a.LocationId == locationId)
-                .Select(a => new
-                {
-                    Title = "Choir Practice in " + (a.Location != null ? a.Location.City : "N/A") + $" (Attendance: {a.Attendees.Count} / {_context.Singers.Count(s => s.SingerLocation.Any(sl => sl.LocationId == locationId))})",
-                    Start = a.StartTime.ToString("yyyy-MM-ddTHH:mm:ss"),
-                    End = a.EndTime.ToString("yyyy-MM-ddTHH:mm:ss")
-                }).ToListAsync();
+		public async Task<JsonResult> GetAttendanceByLocation(int locationId)
+		{
+			var attendanceHistory = await _context.AttendeesSheets
+				.Include(a => a.Director)
+				.Include(a => a.Location)
 
-            return Json(attendanceHistory);
-        }
+				.Include(a => a.Attendees)
+				.Where(a => a.LocationId == locationId)
+				.Select(a => new
+				{
+					Title = "Choir Practice in " + (a.Location != null ? a.Location.City : "N/A") + $" (Attendance: {a.Attendees.Count} / {_context.Singers.Count(s => s.SingerLocation.Any(sl => sl.LocationId == locationId))})",
+					Start = a.StartTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+					End = a.EndTime.ToString("yyyy-MM-ddTHH:mm:ss")
+				}).ToListAsync();
 
-        public JsonResult GetSingerCountByLocation(int locationId)
-        {
-            var singerCount = _context.Singers
-                .Where(s => s.SingerLocation.Any(sl => sl.LocationId == locationId))
-                .Count();
+			return Json(attendanceHistory);
+		}
 
-            return Json(singerCount);
-        }
+		public JsonResult GetSingerCountByLocation(int locationId)
+		{
+			var singerCount = _context.Singers
+				.Where(s => s.SingerLocation.Any(sl => sl.LocationId == locationId))
+				.Count();
 
-        public async Task<JsonResult> GetAllAttendance()
-        {
-            var attendanceHistory = await _context.AttendeesSheets
-                .Include(a => a.Director)
-                .Include(a => a.Location)
-              
-                .Include(a => a.Attendees)
-                .Select(a => new
-                {
-                    Title = "Choir Practice in " + (a.Location != null ? a.Location.City : "N/A") + $" (Attendance: {a.Attendees.Count} / {_context.Singers.Count(s => s.SingerLocation.Any(sl => sl.LocationId == a.LocationId))})",
-                    Start = a.StartTime.ToString("yyyy-MM-ddTHH:mm:ss"),
-                    End = a.EndTime.ToString("yyyy-MM-ddTHH:mm:ss")
-                }).ToListAsync();
+			return Json(singerCount);
+		}
 
-            return Json(attendanceHistory);
-        }
+		public async Task<JsonResult> GetAllAttendance()
+		{
+			var attendanceHistory = await _context.AttendeesSheets
+				.Include(a => a.Director)
+				.Include(a => a.Location)
 
-        public JsonResult GetTotalSingerCount()
-        {
-            var totalSingerCount = _context.Singers.Count();
-            return Json(totalSingerCount);
-        }
+				.Include(a => a.Attendees)
+				.Select(a => new
+				{
+					Title = "Choir Practice in " + (a.Location != null ? a.Location.City : "N/A") + $" (Attendance: {a.Attendees.Count} / {_context.Singers.Count(s => s.SingerLocation.Any(sl => sl.LocationId == a.LocationId))})",
+					Start = a.StartTime.ToString("yyyy-MM-ddTHH:mm:ss"),
+					End = a.EndTime.ToString("yyyy-MM-ddTHH:mm:ss")
+				}).ToListAsync();
 
-        public IActionResult Dashboard()
-        {
-            var viewModel = new DashboardVM
-            {
-                Locations = _context.Locations.ToList()
-            };
+			return Json(attendanceHistory);
+		}
 
-            return View(viewModel);
-        }
+		public JsonResult GetTotalSingerCount()
+		{
+			var totalSingerCount = _context.Singers.Count();
+			return Json(totalSingerCount);
+		}
 
-        private bool AttendanceSheetExists(int id)
-        {
-            return _context.AttendeesSheets.Any(e => e.Id == id);
-        }
-    }
+		public IActionResult Dashboard()
+		{
+			var viewModel = new DashboardVM
+			{
+				Locations = _context.Locations.ToList()
+			};
+
+			return View(viewModel);
+		}
+
+		private bool AttendanceSheetExists(int id)
+		{
+			return _context.AttendeesSheets.Any(e => e.Id == id);
+		}
+	}
 }
