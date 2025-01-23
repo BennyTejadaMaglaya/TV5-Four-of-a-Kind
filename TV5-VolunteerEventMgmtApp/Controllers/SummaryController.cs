704ffa252d8eb22c69e455c83c62463c65e35ccf
Zach <88806017+Zachzzz@users.noreply.github.com>
2025-01-22 20:50:02 -0500

    Basic excel sheet

--- TV5-VolunteerEventMgmtApp/Controllers/SummaryController.cs
+++ TV5-VolunteerEventMgmtApp/Controllers/SummaryController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System.Globalization;
+using System.Linq;
 using TV5_VolunteerEventMgmtApp.Data;
 using TV5_VolunteerEventMgmtApp.Models;
 using TV5_VolunteerEventMgmtApp.Utilities;
@@ -62,8 +65,8 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
                         
 
             var summaryValue = await summary.Select(x => new AttendanceSheetSummaryVM
-            {
-                TotalAttendees = x.Location.SingerLocations.Where(l => l.Singer.isActive).Count(),
+            { // todo when things are fixed we'll set this to only the Total singers and not check the count
+                TotalAttendees = x.TotalSingers != 0 ? x.TotalSingers : x.Location.SingerLocations.Where(l => l.Singer.isActive).Count(),
                 Sheet = x,
                 Percentage = new PercentageColorVM((double)x.Attendees.Count() / x.Location.SingerLocations.Where(l => l.Singer.isActive).Count())
             })
@@ -78,17 +81,94 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
             ViewData["searchDirector"] = searchDirector;
             if (startWeek.HasValue) // todo fix this ( think i
             {
-                ViewData["startWeek"] = startWeek.Value.ToString("yyyy-MM-dd");
+                ViewData["startWeek"] = startWeek.Value.ToString("yyyy-MM-ddTHH:mm");
             }
             if (endWeek.HasValue)
             {
-                ViewData["endWeek"] = endWeek.Value.ToString("yyyy-MM-dd");
+                ViewData["endWeek"] = endWeek.Value.ToString("yyyy-MM-ddTHH:mm");
             }
 
             return View(summaryValue);
 
         }
-        
+
+        [HttpGet]
+        public  IActionResult ThisWeeksExcelReport(string reportTitle="This Weeks Attendance Report")
+        {
+            var summary = ThisWeeksAttendanceExcel();
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                
+
+                var workSheet = excel.Workbook.Worksheets.Add("AttendanceReport");
+                workSheet.Cells[3, 1].LoadFromCollection(summary, true);
+
+                //Add a title and timestamp at the top of the report
+                workSheet.Cells[1, 1].Value = reportTitle;
+                using (ExcelRange Rng = workSheet.Cells[1, 1, 1, 6])
+                {
+                    Rng.Merge = true; //Merge columns start and end range
+                    Rng.Style.Font.Bold = true; //Font should be bold
+                    Rng.Style.Font.Size = 18;
+                    Rng.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                }
+
+                try
+                {
+                    Byte[] theData = excel.GetAsByteArray();
+                    string filename = "Attendance-Summary-2025-01-22.xlsx";
+                    string mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    return File(theData, mimeType, filename);
+                }
+                catch (Exception)
+                {
+                    return BadRequest("Could not build and download the file.");
+                }
+            }
+            
+        }
+
+        private async Task<ICollection<AttendanceSheetSummaryVM>> ThisWeeksAttendance()
+        {
+            var summary = _context.AttendeesSheets
+                .Where(s => s.StartTime >= DateUtilities.GetWeekStart() && s.StartTime <= DateUtilities.GetWeekEnd())
+                .Include(s => s.Director)
+
+                .Include(s => s.Location)
+                .ThenInclude(s => s.SingerLocations)
+                .ThenInclude(s => s.Singer)
+                .AsNoTracking();
+            
+            return await summary.Select(x => new AttendanceSheetSummaryVM
+            { // todo when things are fixed we'll set this to only the Total singers and not check the count
+                TotalAttendees = x.Attendees.Count(),
+                Sheet = x,
+                Percentage = new PercentageColorVM((double)x.Attendees.Count() / x.TotalSingers != 0 ? x.TotalSingers : x.Location.SingerLocations.Where(l => l.Singer.isActive).Count())
+            }).ToListAsync();
+
+        }
+
+        private  ICollection<AttendanceSheetExcel> ThisWeeksAttendanceExcel()
+        { // todo for speeds sake i will clean this up later but im just tryna get this excel shit working
+            var summary = _context.AttendeesSheets
+                .Where(s => s.StartTime >= DateUtilities.GetWeekStart() && s.StartTime <= DateUtilities.GetWeekEnd())
+                .Include(s => s.Director)
+
+                .Include(s => s.Location)
+                .ThenInclude(s => s.SingerLocations)
+                .ThenInclude(s => s.Singer)
+                .AsNoTracking();
+
+            return [.. summary.Select(x => new AttendanceSheetExcel { 
+                AvailableSingers=x.TotalSingers != 0 ? x.TotalSingers : x.Location.SingerLocations.Where(l => l.Singer.isActive).Count(), 
+                DirectorName=x.Director.NameSummary(), 
+                LocationName=x.Location.City,
+                TotalAttendees =x.Attendees.Count(),
+                PercentageAttended= x.TotalSingers != 0 ? ((x.Attendees.Count / x.TotalSingers)).ToString("#0.##%") 
+                :(((double)x.Attendees.Count() / x.Location.SingerLocations.Where(l => l.Singer.isActive).Count())).ToString("#0.##%")
+            })];
+        }
+
 
         private static void FilterDateRange(DateTime? start, DateTime? end, ref IQueryable<AttendanceSheet> sheets)
         {
