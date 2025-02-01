0dbd5cc448313e89beb9c365a02f434dc3e064d7
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-02-01 06:54:32 -0500

    Added ViewBag.AvailableLocations to populate the dropdown…

--- TV5-VolunteerEventMgmtApp/Controllers/ModalController.cs
+++ TV5-VolunteerEventMgmtApp/Controllers/ModalController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TV5_VolunteerEventMgmtApp.Data;
 
 namespace TV5_VolunteerEventMgmtApp.Controllers
 {
     public class ModalController : Controller
     {
+        private readonly VolunteerEventMgmtAppDbContext _context;
+
+        public ModalController(VolunteerEventMgmtAppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +26,7 @@ namespace TV5_VolunteerEventMgmtApp.Controllers
             {
                 return new BadRequestResult();
             }
+            ViewBag.AvailableLocations = new SelectList(_context.Locations.Where(l => l.IsActive), "ID", "City");
 
             return PartialView($"~/Views/Shared/{partialViewName}.cshtml");
         }
